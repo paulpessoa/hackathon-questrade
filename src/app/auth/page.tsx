89746b1897be501740c0c3a8879c2afad93ef726
Paul Pessoa <paulmspessoa@gmail.com>
2023-07-09 13:56:12 -0300

    home and auth page

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,30 @@
+import './style.scss'
+import React from "react";
+
+function Auth () {
+  return (  
+      
+      <div className="form">
+      <div className="formLogin">
+        {/* <form onSubmit={submitLogin}> */}
+        <form>
+        <h2>Welcome</h2>
+            <input className="input_mail" type="email" placeholder="e-mail"/>
+            <input className="input_pass" type="password" placeholder="password"/>
+            <button className="buttonStandard btn_Login" type = 'submit'>Login</button>
+            <ul>
+                <li><a className="linkStandard" href="/About">Register</a></li>
+            </ul>
+        </form>
+        </div>
+      </div>
+      
+    );
+
+    // function submitLogin (event) {
+    //     event.preventDefault();
+    //     alert('Welcome!!!!')
+    // }
+  };
+  
+  export default Auth;
